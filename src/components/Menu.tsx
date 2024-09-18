af16c97f54ac9158d7518b4593f501efa82796e5
BirdOfSteel <AmirAziz2001@Gmail.com>
2024-09-18 20:33:29 +0100

    Changed styling for menu
    - Menu now has a glassy, blurred background and a new arrow icon.
    
    - Fixed menu dimensions. Now more responsive.
    
    - Updated Typescript from 5.5.4 to 5.6.2.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,37 +1,45 @@
 import React from 'react';
 import menuArrowIcon from '../assets/down-arrow.png';
+import arrowUpLeft from '../assets/arrow-up-left.svg';
 import MenuButtons from './MenuButtons.tsx';
 
 export default function Menu({ isMenuOpen, setIsMenuOpen, units, setUnits}) {
     const toggleMenuStyle = {
-        width: isMenuOpen ? '35%' : '5%',
-        height: isMenuOpen ? '35%' : '45px'
+        width: isMenuOpen ? '290px' : '0px',
+        height: isMenuOpen ? '210px' : '10px',
+        background: 'rgba(240, 240, 240, 0)',
+        boxShadow: isMenuOpen ? 
+            'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px' : 
+            ''
     }
     
+    const rotateMenuIconStyle = {
+        transform: isMenuOpen ? 
+            'rotate3d(0,0,1,360deg)' : 
+            'rotate3d(0,0,-1,180deg)'
+    }
+
     const fadeStyle = {
         opacity: isMenuOpen ? 1 : 0,
-        transition: 'opacity 0.25s ease-in-out'
+        transition: 'opacity 0.25s ease-in-out',
     };
 
     return (
         <div 
             id="menu-div"
             style={toggleMenuStyle}>
-
-            <img 
-                id='menu-icon' 
-                src={menuArrowIcon} 
-                style={isMenuOpen ? {'transform': 'rotate3d(0,0,-1,45deg)'} : {'transform': 'rotate3d(0,0,1,-225deg)'} }
-                onClick={() => setIsMenuOpen((prevState: boolean) => {
-                    return !prevState
-                })}
-            />
-
             <div id="menu-list-div" style={fadeStyle}>
                 <MenuButtons units={units} setUnits={setUnits} />
             </div>
 
-
+            <img
+                src={arrowUpLeft}
+                style={rotateMenuIconStyle}
+                id="menu-toggle-button"
+                onClick={() => setIsMenuOpen((prevState: boolean) => {
+                    return !prevState;
+                })}
+            />
         </div>
     )
 }
